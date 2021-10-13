4d3b7858a568a0465a325ef3fec8de6cb79512cc
User 12 <User 12@PC17>
2021-10-13 14:44:03 +0300

    Add project files.

---
+++ algoritum2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace algoritum2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a != b)
+            {
+                if (a > b)    
+                {
+                    a = a - b;
+
+                   
+                }
+                else
+                {
+                    b = b - a;
+                }
+                Console.WriteLine("най голям общ делител е:" + a);
+            }
+            
+        }
+    }
+}
